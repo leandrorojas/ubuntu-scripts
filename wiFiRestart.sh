03864a35e80e8c84addd3fe8f2ce674557af57ab
leandrorojas <leandrorojas@gmail.com>
2024-08-11 23:31:10 -0300

    Updated wiFiRestart.sh
    added comment in line 3

--- wiFiRestart.sh
+++ wiFiRestart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# some WiFi radio hardware needs to be restarted in ubuntu so it can be used properly... this is a workaround to an unknown underlying cause
+
 echo "turning OFF wifi radio"
 nmcli radio wifi off
 echo "waiting 3s until re-enabling"
